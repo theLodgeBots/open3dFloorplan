7f0998bd6cbdbc28cc0e48c6f36dc0fcb9f5493c
matijagaspar <matija.gaspar@gmail.com>
2026-08-25 22:06:13 +0200

    chore: add vercel deployment support, only used on vercel

--- svelte.config.js
+++ svelte.config.js
@@ -1,10 +1,6 @@
-import adapter from '@sveltejs/adapter-node';
+import node from '@sveltejs/adapter-node';
+import vercel from '@sveltejs/adapter-vercel';
 
-/** @type {import('@sveltejs/kit').Config} */
-const config = {
-	kit: {
-		adapter: adapter()
-	}
+export default {
+  kit: { adapter: process.env.VERCEL ? vercel() : node() }
 };
-
-export default config;
